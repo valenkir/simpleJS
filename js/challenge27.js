6e72034b20bbaf8b6db85aa16fa48c3f979cb788
Valentyna Boriak <valenkiri@gmail.com>
2024-04-03 19:40:32 +0300

    Add solutions for all tasks except for the 'next date' task

--- js/challenge27.js
+++ js/challenge27.js
@@ -1,13 +1,15 @@
 function isValidNumber(promptNumber) {
-  return !isNaN(promptNumber) && promptNumber !== null;
+  return (
+    !isNaN(promptNumber) && promptNumber !== null && promptNumber.trim().length
+  );
 }
 
-function isValidAge(promptNumber) {
+function isValidPositiveNumber(promptNumber) {
   return isValidNumber(promptNumber) && Math.sign(promptNumber) > 0;
 }
 
 function isValidString(promptString) {
-  return promptString && promptString.trim();
+  return promptString.length > 0 && promptString.trim();
 }
 
 //for any number of chars in a string (hopefully)
@@ -20,11 +22,12 @@ function findIdenticalChars(str) {
       }
     }
   }
-  if (identicalChars.length) {
-    return identicalChars;
-  } else {
-    return false;
-  }
+  return identicalChars;
+}
+
+function isNumberOfDigitsCorrect(numberOfDigits, numberToCheck) {
+  numberToCheck += "";
+  return numberToCheck.trim().length === numberOfDigits ? true : false;
 }
 
 //3 DIGITS
@@ -33,10 +36,10 @@ const digitsField = document.querySelector(".digits-field");
 
 digitsBtn.addEventListener("click", () => {
   let numberToCheck = prompt("Enter a 3-digit number:");
-  if (isValidNumber(numberToCheck) && !Math.trunc(numberToCheck / 1000)) {
-    if (numberToCheck % 1 !== 0) {
-      numberToCheck = Math.trunc(Number(numberToCheck));
-    }
+  if (
+    isValidNumber(numberToCheck) &&
+    isNumberOfDigitsCorrect(3, numberToCheck)
+  ) {
     numberToCheck = Math.abs(numberToCheck) + "";
 
     //remove the duplicated values
@@ -60,12 +63,12 @@ const palindromeField = document.querySelector(".palindrome-field");
 palindromeBtn.addEventListener("click", () => {
   let numberToCheck = prompt("Enter a 5-digit number:");
 
-  //division by 100000 doesn't seem like a proper way to do it
-  if (isValidNumber(numberToCheck) && !Math.trunc(numberToCheck / 100000)) {
-    numberToCheck = Math.trunc(Number(numberToCheck));
+  if (
+    isValidNumber(numberToCheck) &&
+    isNumberOfDigitsCorrect(5, numberToCheck)
+  ) {
     numberToCheck = Math.abs(numberToCheck) + "";
 
-    //TODO: write a function for any number of digits
     if (
       numberToCheck.charAt(0) === numberToCheck.charAt(4) &&
       numberToCheck.charAt(1) === numberToCheck.charAt(3)
@@ -79,4 +82,85 @@ palindromeBtn.addEventListener("click", () => {
   }
 });
 
-//
+//CURRENCY CONVERTER
+const amountField = document.querySelector(".currency-amount-field");
+const currencyDropDown = document.querySelector(".choose-currency-dropdown");
+const converterBtn = document.querySelector(".converter-btn");
+const converterResultField = document.querySelector(".converter-result-field");
+const errorLabel = document.querySelector(".converter-error-label");
+
+const rates = {
+  EUR: 0.92,
+  UAH: 39.13,
+  BGN: 1.81,
+};
+
+converterBtn.addEventListener("click", () => {
+  const amount = amountField.value;
+
+  if (isValidPositiveNumber(amount)) {
+    const selectedCurrency =
+      currencyDropDown.options[currencyDropDown.selectedIndex].text;
+    for (const currency in rates) {
+      if (currency === selectedCurrency)
+        converterResultField.value = `${+amount * rates[currency]} (rate: ${
+          rates[currency]
+        })`;
+    }
+    errorLabel.innerHTML = "";
+  } else {
+    errorLabel.innerHTML = "Enter a valid number";
+    converterResultField.value = "";
+  }
+});
+
+//CIRCLE INSCRIBED IN SQUARE
+const circumferenceField = document.querySelector(".circumference-field");
+const perimeterField = document.querySelector(".perimeter-field");
+const checkInscribedCircleBtn = document.querySelector(
+  ".check-inscribed-circle-btn"
+);
+const inscribedCircleResultField = document.querySelector(
+  ".inscribed-circe-result-field"
+);
+const errorLabelInscribedCircle = document.querySelector(
+  ".inscribed-circle-error-label"
+);
+
+checkInscribedCircleBtn.addEventListener("click", () => {
+  const circumference = circumferenceField.value;
+  const perimeter = perimeterField.value;
+  const pi = 3.14;
+  if (
+    isValidPositiveNumber(circumference) &&
+    isValidPositiveNumber(perimeter)
+  ) {
+    const sideLength = +perimeter / 4;
+    const diameter = +circumference / pi;
+    inscribedCircleResultField.value =
+      sideLength === diameter
+        ? "Circle is inscribed in square"
+        : "Circle is not inscribed in square";
+    errorLabelInscribedCircle.innerHTML = "";
+  } else {
+    errorLabelInscribedCircle.innerHTML = "Enter a valid number";
+    inscribedCircleResultField.value = "";
+  }
+});
+
+//EMAIL VALIDATION
+const emailField = document.querySelector(".email-field");
+const emailErrorLabel = document.querySelector(".email-error-label");
+const emailSubmitBtn = document.querySelector(".email-submit-btn");
+
+emailSubmitBtn.addEventListener("click", () => {
+  const email = emailField.value;
+  if (isValidString(email)) {
+    if (email.length > 20 || !email.includes("@")) {
+      emailErrorLabel.innerHTML =
+        "The email should include @ and the number of characters should be <20";
+    }
+  } else {
+    emailErrorLabel.innerHTML = "The email should not be empty";
+  }
+});
